1cb93e9602e78fa359a5590b83f2f1f43e747906
Robert Petricevic <robopetricevic@gmail.com>
2020-10-28 19:16:17 +0100

    WorshopList page layout

--- src/pages/WorkshopsList.js
+++ src/pages/WorkshopsList.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 const WorkshopsList = (props) => {
-  return <div>WorkshopsList</div>;
+  return (
+    <div className="workshopPage">
+      <p className="filterBy">Filter by category:</p>
+      <div className="infoBox">
+        <p className="pageTitle">Workshops</p>
+        <p className="displayed">Displayed: 12</p>
+      </div>
+      <div className="categoriesBox">
+        <p className="category all">All</p>
+        <p className="category">
+          <i class="fas fa-paint-brush categoryIcon"></i>Design
+        </p>
+        <p className="category">
+          <i class="fas fa-desktop categoryIcon"></i>Frontend
+        </p>
+        <p className="category">
+          <i class="fas fa-code categoryIcon"></i>Backend
+        </p>
+        <p className="category">
+          <i class="fas fa-bolt categoryIcon"></i>Marketing
+        </p>
+      </div>
+      <div className="main"></div>
+    </div>
+  );
 };
 
 export default WorkshopsList;
